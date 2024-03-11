0a1d73834ac81d4848ecc99e929664c57d291121
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-03-12 12:48:32 +1300

    Add catch all route to return home.

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import DefaultLayout from '../layouts/DefaultLayout.vue'
+</script>
+
+<template>
+  <default-layout>
+    <div class="not-found">
+      <h1>Not Found</h1>
+      <p>Page not found.</p>
+      <router-link :to="{ name: 'home' }">Return home
+      </router-link>
+    </div>
+  </default-layout>
+</template>
